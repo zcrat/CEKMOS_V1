e4e05921570994ea2e8bbfb8af86d7e3e57c430d
Zcrat <jchugut@gmil.com>
2026-03-20 00:31:01 -0600

    AJUSTES

--- app/Http/Controllers/select2controller.php
+++ app/Http/Controllers/select2controller.php
@@ -30,11 +30,13 @@ class select2controller extends Controller
     }
     public function VehiculosConceptosPorModulo(Request $request){
 
-        $validation=Validator::make($request->all(),['id_modulo'=>['required','exists:modulo_ordenes_servicios,id']]);
+        $validation=Validator::make($request->all(),['id_modulo'=>['required','exists:modulo_ordenes_servicios,id'],'query'=>['string']]);
         $options=[];
         if (!$validation->fails()){
+            $search="%".$validation->validated()['query']."%";
             $options=VehiculosConceptos::join('vehiculos_conceptos_disponibles','vehiculos_conceptos_disponibles.vehiculo_concepto_id','=','vehiculos_conceptos.id')
             ->where('vehiculos_conceptos_disponibles.modulo_orden_id',$validation->validated()['id_modulo'])
+            ->where('descripcion','LIKE',$search)
             ->get()->map(fn($item)=> [
             'value'=>$item->id,
             'label'=>$item->descripcion
