ed29b9544f85b7d25905e4f08aefbe77e71670f6
Zcrat <jchugut@gmil.com>
2026-04-24 21:36:19 -0600

    correcion de tipado

--- resources/js/components/Zcrat/Forms/TiposVehiculos.vue
+++ resources/js/components/Zcrat/Forms/TiposVehiculos.vue
@@ -3,6 +3,7 @@
     import { ref,onMounted} from 'vue' ;
     import {type option} from '@/types/generales';
     import axios from 'axios' 
+import Label from '@/components/ui/label/Label.vue';
 
     const tiposvehiculos=ref<option[]>([]);
     const SelectValue=defineModel<number | null>();
@@ -10,7 +11,10 @@
     onMounted(async () => {
         try {
             const response = await axios.get(route('select.tipos.vehiculos'))
-            tiposvehiculos.value=response.data.options;
+            tiposvehiculos.value=response.data.options.map((item:option)=>{return {
+                value:Number(item.value),
+                label:item.label,
+            }});
         } catch (error: any) {
             tiposvehiculos.value=[]
         } 
