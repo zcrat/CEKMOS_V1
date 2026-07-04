d4425114df70656999785899a16ee42671901419
Zcrat <jchugut@gmil.com>
2026-07-04 16:46:25 -0600

    ajuste al login mejorando la estetica de la imagen

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -70,7 +70,7 @@ const submit = () => {
             </div>
 
             <div class="flex items-center justify-between mt-4 sm:mb-0 mb-12">
-                <Link :href="route('password.request')" class="text-[0.8rem] sm:text-sm underline  text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <Link :href="route('password.request')" class="text-[0.8rem] sm:text-sm underline  text-gray-400 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Olvidaste TU Contraseña?
                 </Link>
 
--- resources/js/components/AuthenticationCard.vue
+++ resources/js/components/AuthenticationCard.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="min-h-screen flex justify-center items-center pt-6 sm:pt-0 bg-[url(/storage/Logos/Sistema/Fondo_Principal.png)] bg-no-repeat bg-cover ">
-        <div class="flex w-full sm:w-[40rem] sm:h-[40rem] flex-col rounded-full bg-white  bg-tr items-center justify-center overflow-hidden">
-        <div class="sm:w-[30rem] w-[10rem] sm:p-0 pt-4">
-            <slot name="logo" />
-        </div>
+        <div class="flex w-[30rem] sm:h-[40rem] h-full flex-col bg-[--color_logo] rounded-lg items-center justify-center p-2 relative border">
+            <div class="rounded-lg overflow-hidden h-full">
+                <slot name="logo" />
+            </div>
 
-        <div class="sm:w-[30rem] w-[18rem] sm:mt-6  bg-white overflow-hidden sm:rounded-lg">
-            <slot />
-        </div>
+            <div class="w-full">
+                <slot />
+            </div>
          </div>
     </div>
 </template> 
